0c42ed6686453501afd0115099190937f328f661
IsamaDes <isamadesmond@gmail.com>
2025-11-23 11:52:37 +0100

    added redis configuration for storing data in memory

---
+++ src/config/index.ts
@@ -0,0 +1,7 @@
+export const config = {
+port: process.env.PORT ? Number(process.env.PORT) : 4000,
+legacyBaseUrl: process.env.LEGACY_BASE_URL || 'https://jsonplaceholder.typicode.com',
+cacheTtlSeconds: Number(process.env.CACHE_TTL),
+redisUrl: process.env.REDIS_URL || 'redis://localhost:6379',
+}
+
